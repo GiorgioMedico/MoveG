9bc8d546b9a7936383770f2b2321c7d8b95e4332
GiorgioMedico <giorgio.medico11@gmail.com>
2024-11-06 14:01:50 +0000

    GiorgioMedico published a site update

--- search/all_7.js
+++ search/all_7.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['operator_2a_25',['operator*',['../classMoveG_1_1Pose.html#ac2d8d8d34943baf3b80d83d8e4e17a6d',1,'MoveG::Pose::operator*()'],['../classMoveG_1_1Rotation.html#a061a42aa21dd629d85d4a406f11fbde1',1,'MoveG::Rotation::operator*()']]],
   ['operator_3c_3c_26',['operator&lt;&lt;',['../classMoveG_1_1Pose.html#a4cce36480a3cc647a721b8e727459156',1,'MoveG::Pose::operator&lt;&lt;()'],['../classMoveG_1_1Pose.html#a4cce36480a3cc647a721b8e727459156',1,'MoveG::Pose::operator&lt;&lt;()'],['../namespaceMoveG.html#a631c2327b394df65bee588669036ee5d',1,'MoveG::operator&lt;&lt;()']]],
-  ['operator_3d_27',['operator=',['../classMoveG_1_1Pose.html#a5145e83febc33105d0801af6c5f8703c',1,'MoveG::Pose::operator=()'],['../classMoveG_1_1Rotation.html#a98c968c68b01b721d6657f1ad683bc0a',1,'MoveG::Rotation::operator=()']]]
+  ['operator_3d_27',['operator=',['../classMoveG_1_1Pose.html#a5145e83febc33105d0801af6c5f8703c',1,'MoveG::Pose::operator=()'],['../classMoveG_1_1Rotation.html#a98c968c68b01b721d6657f1ad683bc0a',1,'MoveG::Rotation::operator=(const Rotation &amp;other)'],['../classMoveG_1_1Rotation.html#a91983145ccf033ea1352927b0ff1af48',1,'MoveG::Rotation::operator=(Rotation &amp;&amp;other) noexcept']]]
 ];
