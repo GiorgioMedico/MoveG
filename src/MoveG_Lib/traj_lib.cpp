d0080e0161933a297723f1e2903448a238cebc59
Giorgio Medico <giorgio.medico@studio.unibo.it>
2025-03-01 11:33:51 +0100

    traj from 0

--- src/MoveG_Lib/traj_lib.cpp
+++ src/MoveG_Lib/traj_lib.cpp
@@ -3,5 +3,5 @@
  * @brief Classe per la rappresentazione di Traiettorie
  *
  * @author Giorgio Medico
- * @date 5/10/2024
+ * @date 01/03/2025
  */
--- src/MoveG_Lib/traj_lib.h
+++ src/MoveG_Lib/traj_lib.h
@@ -3,7 +3,7 @@
  * @brief Classe per la rappresentazione di Traiettorie
  *
  * @author Giorgio Medico
- * @date 5/10/2024
+ * @date 01/03/2025
  */
 
 #pragma once
@@ -22,37 +22,5 @@
 
 namespace MoveG
 {
-// Workspace Representation
-
-// Classe Waypoint per la rappresentazione di un punto di passaggio con velocità e accelerazione
-class Waypoint
-{
-public:
-    // Costruttori
-    Waypoint();
-
-    // Distruttore
-    ~Waypoint();
-
-    // Metodi di accesso
-
-
-private:
-    Eigen::Vector3d position_;
-    Eigen::Quaterniond orientation_;
-    Eigen::Vector3d velocity_;
-    Eigen::Vector3d acceleration_;
-};
-
 
 } // namespace MoveG
-
-// Classe Trajectory per la rappresentazione di una traiettoria nel workspace
-
-
-// Joint Space Representation
-
-
-// Classe JointWaypoint per la rappresentazione di un punto di passaggio nello spazio delle giunzioni
-
-// Classe JointTrajectory per la rappresentazione di una traiettoria nello spazio delle giunzioni
